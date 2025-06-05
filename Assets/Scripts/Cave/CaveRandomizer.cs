fc9cb83a7688f540d0b24ce9a4e8596a0e6f6ed2
chihyun7 <clgus1603@gmail.com>
2025-06-05 22:22:47 +0900

    동굴위치 3곳중 1곳 랜덤 생성및 저장후 동굴위치 동일하게 저장

---
+++ Assets/Scripts/Cave/CaveRandomizer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class CaveRandomizer : MonoBehaviour
+{
+    public GameObject[] caveSpawns; // 동굴 오브젝트 3개 등록할 배열
+
+    void Start()
+    {
+        int isContinue = PlayerPrefs.GetInt("IsContinue", 0);
+
+        if (isContinue == 0)
+        {
+            // 새 게임이면 랜덤으로 하나만 활성화
+            int selectedIndex = Random.Range(0, caveSpawns.Length);
+
+            for (int i = 0; i < caveSpawns.Length; i++)
+            {
+                caveSpawns[i].SetActive(i == selectedIndex);
+            }
+
+            Debug.Log($" 새 게임 → 동굴 {selectedIndex + 1}번만 활성화됨");
+        }
+    }
+}
